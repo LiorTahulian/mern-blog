bcd1ea0692bc0d39a7d6bc38989942e540e55fbb
lior <lior.t95@gmail.com>
2024-05-12 21:22:16 +0300

    create sign up page UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/no-unescaped-entities */
 import { Button, Navbar, TextInput } from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
@@ -13,7 +12,7 @@ export default function Header() {
         className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
       >
         <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-          Lior's
+          Lior&apos;s
         </span>
         Blog
       </Link>
